6a0bdca8f5bc641389885a3356f5b239306db45e
Biju Punalor <biju.punalor@gmail.com>
2024-05-09 07:10:38 +0530

    test Post class

--- test_Still.py
+++ test_Still.py
@@ -3,7 +3,7 @@
 import unittest 
 
 class TestPost(unittest.TestCase):
-    
+
     def test_markdown(self):
         from markdown_it import MarkdownIt
         commonmark = MarkdownIt('commonmark')
@@ -11,3 +11,21 @@ class TestPost(unittest.TestCase):
         text = "**bold text**"
         result = commonmark.render(text)
         self.assertEqual(result, "<p><strong>bold text</strong></p>\n")
+
+    def test_frontmatter(self):
+        import tomllib
+
+        data = """
+            name = "python-Still"
+            description = "convert markdown to html"
+        """
+
+        result = tomllib.loads(data)
+        self.assertDictEqual(result, {'name': 'python-Still', 'description': 'convert markdown to html'})
+
+class TestPage(unittest.TestCase):
+    pass
+
+class TestSite(unittest.TestCase):
+    pass
+
